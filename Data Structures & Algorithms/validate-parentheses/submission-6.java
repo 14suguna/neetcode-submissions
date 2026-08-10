71ea7b0fe75200b74f1bc702bfd76764a5fa5348
14suguna <93744595+14suguna@users.noreply.github.com>
2026-08-10 16:51:39 +0100

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> charc = new Stack<>();
+
+       // s="[(])"
+
+        for(int i=0; i<s.length();i++){
+            if(s.charAt(i)== '{' || s.charAt(i)=='[' || s.charAt(i)=='(')
+            {
+                charc.push(s.charAt(i));
+            }else if(charc.isEmpty()){
+                    return false;
+                }
+            // else if(s.charAt(i)=='}' || s.charAt(i)==']' || s.charAt(i)==')')
+            else if(s.charAt(i)=='}' && charc.peek() == '{'){
+                charc.pop();
+                }
+            else if(s.charAt(i)==')' && charc.peek() == '('){
+                charc.pop();
+                }
+            else if(s.charAt(i)==']' && charc.peek() == '['){
+                charc.pop();
+                }
+            else{
+                return false;
+            }
+        }
+        if(!charc.isEmpty()){
+                    return false;
+                }
+
+            return true;
+    }
+}
